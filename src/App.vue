b58b1ea96858f084b8092c6dc1f6b77bbee1d674
Emil Mattsson <EmilMattsson@github.com>
2020-11-19 20:05:48 +0100

    a start

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app" v-bind:style="styleObject">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <button @click="togglePalette()">
+      {{ this.styleObject.background === 'darkgray' ? 'light' : 'dark' }}
+    </button>
+    <HelloWorld msg="Hello there..!" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: () => {
+    return {
+      styleObject: {
+        background: 'darkgray',
+        color: 'white'
+      }
+    }
+  },
+  methods: {
+    togglePalette() {
+      this.darkMode = !this.darkMode
+      this.styleObject.background === 'darkgray'
+        ? this.styleObject = {
+          background: 'white',
+          color: 'darkgray'
+        }
+        : this.styleObject = {
+          background: 'darkgray',
+          color: 'white'
+        }
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
+</style>
